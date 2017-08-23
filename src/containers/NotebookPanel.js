33cb2102f18e6357807bdb28fe8a13063fb7b267
Jiahuan He <jiahuan_he@outlook.com>
2017-08-23 02:46:33 -0600

    Add inserting transaction

--- src/containers/NotebookPanel.js
+++ src/containers/NotebookPanel.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import NotebookItem from '../components/NotebookItem'
-import { Grid, List } from 'semantic-ui-react'
+import {  Grid, List, Transition, Input, } from 'semantic-ui-react'
 import NotebookPanelHeader from './NotebookPanelHeader';
 
 class NotebookPanel extends React.Component{
+
+
+    state = {
+        notebooks: [
+            <NotebookItem title="Notebook title" createDate="2017 Aug 10"/>],
+        visible: false
+    };
+
+    handleAdd = () =>
+        {
+            this.setState(
+                {notebooks: this.state.notebooks.concat(<NotebookItem title="Notebook title" createDate="2017 Aug 10"/>) }
+            );
+        console.log("handleAdd");
+    };
+
 
     //TODO
     // Title
@@ -15,12 +31,21 @@ class NotebookPanel extends React.Component{
 
         return (
             <Grid.Column width={this.props.width}>
-                <NotebookPanelHeader/>
-                <List.List >
-                    <NotebookItem title="Notebook title" createDate="2017 Aug 10"/>
-                    <NotebookItem title="Notebook title" createDate="2017 Aug 10"/>
-                    <NotebookItem title="Notebook title" createDate="2017 Aug 10"/>
-                </List.List>
+                <NotebookPanelHeader onClick={this.handleAdd}/>
+                    <List.List >
+                        <Transition.Group
+                            as={List}
+                            duration={500}
+                            divided
+                            size='large'
+                            verticalAlign='middle'>
+                            {this.state.notebooks.map(item => (
+                                <List.Item key={item.id}>
+                                    {item}
+                                </List.Item>
+                            ))}
+                        </Transition.Group>
+                    </List.List>
             </Grid.Column>
         );
     }
--- src/containers/NotebookPanelHeader.js
+++ src/containers/NotebookPanelHeader.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import { Button, Icon } from 'semantic-ui-react'
 
-const NotebookPanelHeader = () => (
-    <div>
-        <Button toggle={false} className='panel-header-button' fluid animated="vertical">
-            <Button.Content hidden >ADD NOTEBOOK</Button.Content>
-            <Button.Content visible>
-                <Icon name='plus' />
-            </Button.Content>
-        </Button>
-    </div>
-);
+class NotebookPanelHeader extends React.Component{
+
+    render(){
+    return (
+        <div onClick={this.props.onClick}>
+            <Button toggle={false} className='panel-header-button' fluid animated="vertical">
+                <Button.Content hidden >ADD NOTEBOOK</Button.Content>
+                <Button.Content visible>
+                    <Icon name='plus' />
+                </Button.Content>
+            </Button>
+        </div>
+    )
+    }
+};
 
 export default NotebookPanelHeader;
