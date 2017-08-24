52650f6f742b9dc5139ff712235853e4f75c4191
Jiahuan He <jiahuan_he@outlook.com>
2017-08-23 19:49:24 -0600

    Refactor and apply combineReducer

--- src/reducers/notebookReducer.js
+++ src/reducers/notebookReducer.js
@@ -1,34 +1,21 @@
 import {ACTION} from "../constants/type";
 
 
-
-const initState = {
-    notebooks: [],
-    pages: [],
-};
-
-// export const notebookReducer = (state={notebooks: []}, action) => {
-//     console.log(initState.pages);
-//     switch (action.type){
-//         case ACTION.NOTEBOOK_ADD:
-//             console.log("new notebook");
-//             return state;
-//         default:
-//             return state;
-//     }
-// };
-
-export const pageReducer = (state={pages: [], notebooks: []}, action) => {
+export const notebooks = (state = [], action) => {
     switch (action.type){
         case ACTION.NOTEBOOK_ADD:
+            return [...state, action.payload];
+        default:
+            return state;
+    }
+};
 
+export const pages = (state= [], action) => {
+    switch (action.type){
+        case ACTION.NOTEBOOK_ADD:
             return state;
         case ACTION.PAGE_ADD:
-
-            return {...state, pages: state.pages.concat({
-                title: action.payload.title,
-                createDate: action.payload.createDate
-            })} ;
+            return [...state, action.payload];
         default:
             return state;
     }
--- src/reducers/store.js
+++ src/reducers/store.js
@@ -1,14 +1,14 @@
 import { combineReducers,applyMiddleware } from 'redux'
 import { createStore } from 'redux'
-import { pageReducer} from "./notebookReducer";
+import { notebooks, pages} from "./notebookReducer";
 import logger from 'redux-logger'
 
 
 
 
-// const reducer = combineReducers({notebookReducer, pageReducer});
+const reducer = combineReducers({notebooks, pages});
 
-const store = createStore( pageReducer, applyMiddleware(logger));
+const store = createStore( reducer, applyMiddleware(logger));
 console.log(store.getState());
 
 export default store;
